ac45caca172b4d262d9ff25e70d7c6f4eacb289d
lyduybach2612 <lyduybach900@gmail.com>
2024-10-25 12:30:03 +0700

    give permission for double value

--- src/GUI.java
+++ src/GUI.java
@@ -11,7 +11,7 @@ public class GUI extends JFrame {
     private final Queue<Process> processes = new PriorityQueue<>(Comparator.comparing(Process::getArrivalTime));
 
     public GUI() {
-        this.setSize(700,500);
+        this.setSize(900,500);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
@@ -22,19 +22,19 @@ public class GUI extends JFrame {
     public void initializeComponent() {
 
         JLabel lb_id = new JLabel("ID:");
-        JLabel lb_arrivalTime = new JLabel("Thời gian đến:");
-        JLabel lb_burstTime = new JLabel("Thời gian thực thi:");
+        JLabel lb_arrivalTime = new JLabel("Arrival Time:");
+        JLabel lb_burstTime = new JLabel("Burst Time:");
 
         JTextField tf_id = new JTextField();
         JTextField tf_arrivalTime = new JTextField();
         JTextField tf_burstTime = new JTextField();
         JButton btn_add  = new JButton("Thêm");
         JButton btn_execute = new JButton("SJF");
-        String[] headers = {"ID","Thời gian đến","Thời gian thực thi", "Thời gian chờ", "Thời gian bắt đầu"};
+        String[] headers = {"ID","Arrival Time","Burst Time", "Waiting Time", "Start Time", "Completion Time", "Turnaround Time"};
         DefaultTableModel model = new DefaultTableModel(headers,0);
         JTable table = new JTable(model);
         JPanel panel = new JPanel(new GridLayout(1,1));
-        panel.setBounds(50,115, 600,300);
+        panel.setBounds(50,115, 800,300);
         panel.add(new JScrollPane(table));
         JButton btn_delete = new JButton("Xoá");
 
@@ -49,11 +49,11 @@ public class GUI extends JFrame {
 
                 try {
 
-                    if(Integer.parseInt(tf_burstTime.getText()) < 0 || Integer.parseInt(tf_arrivalTime.getText()) < 0){
+                    if(Double.parseDouble(tf_burstTime.getText()) < 0 || Double.parseDouble(tf_arrivalTime.getText()) < 0){
                         throw new RuntimeException("Number is invalid");
                     }
-                    processes.add(new Process(Integer.parseInt(tf_id.getText()), Integer.parseInt(tf_arrivalTime.getText()), Integer.parseInt(tf_burstTime.getText())));
-                    String[] row = {tf_id.getText(),tf_arrivalTime.getText(),tf_burstTime.getText(), "0", "0"};
+                    processes.add(new Process(Integer.parseInt(tf_id.getText()), Double.parseDouble(tf_arrivalTime.getText()), Double.parseDouble(tf_burstTime.getText())));
+                    String[] row = {tf_id.getText(),tf_arrivalTime.getText(),tf_burstTime.getText(), "0", "0", "0", "0"};
                     model.addRow(row);
 
 
@@ -79,7 +79,15 @@ public class GUI extends JFrame {
                 List<Process> processList = executeSJF(processes);
                 model.setRowCount(0);
                 for (Process p : processList) {
-                    String[] row = {String.valueOf(p.getId()), String.valueOf(p.getArrivalTime()), String.valueOf(p.getBurstTime()), String.valueOf(p.getWaitingTime()), String.valueOf(p.getStartTime())};
+                    String[] row = {
+                            String.valueOf(p.getId()),
+                            String.valueOf(p.getArrivalTime()),
+                            String.valueOf(p.getBurstTime()),
+                            String.valueOf(p.getWaitingTime()),
+                            String.valueOf(p.getStartTime()),
+                            String.valueOf(p.getCompletionTime()),
+                            String.valueOf(p.getTurnaroundTime())
+                    };
                     model.addRow(row);
                 }
 
@@ -120,7 +128,7 @@ public class GUI extends JFrame {
     public List<Process> executeSJF(Queue<Process> processes){
 
         Queue<Process> readyQueue = new PriorityQueue<>(Comparator.comparing(Process::getBurstTime));
-        int currentTime = 0;
+        double currentTime = 0;
         List<Process> processList = new ArrayList<>();
         while (!processes.isEmpty() || !readyQueue.isEmpty()) {
 
@@ -130,12 +138,14 @@ public class GUI extends JFrame {
 
             if (!readyQueue.isEmpty()) {
                 Process currentProcess = readyQueue.poll();
-                currentProcess.setWaitingTime(currentTime - currentProcess.getArrivalTime());
                 currentProcess.setStartTime(currentTime);
-                processList.add(currentProcess);
                 currentTime += currentProcess.getBurstTime();
+                currentProcess.setCompletionTime();
+                currentProcess.setTurnaroundTime();
+                currentProcess.setWaitingTime();
+                processList.add(currentProcess);
             } else {
-                currentTime++;
+                currentTime += Integer.parseInt(String.valueOf(currentTime)) + 1;
             }
         }
         return processList;
--- src/Process.java
+++ src/Process.java
@@ -1,20 +1,24 @@
 public class Process {
 
     private int id;
-    private int arrivalTime;
-    private int burstTime;
-    private int waitingTime;
-    private int startTime;
+    private double arrivalTime;
+    private double burstTime;
+    private double waitingTime;
+    private double startTime;
+    private double turnaroundTime;
+    private double completionTime;
 
     public Process() {
     }
 
-    public Process(int id, int arrivalTime, int burstTime) {
+    public Process(int id, double arrivalTime, double burstTime) {
         this.id = id;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
         this.waitingTime = 0;
         this.startTime = 0;
+        this.turnaroundTime = 0;
+        this.completionTime = 0;
     }
 
     public int getId() {
@@ -25,36 +29,52 @@ public class Process {
         this.id = id;
     }
 
-    public int getArrivalTime() {
+    public double getArrivalTime() {
         return arrivalTime;
     }
 
-    public void setArrivalTime(int arrivalTime) {
+    public void setArrivalTime(double arrivalTime) {
         this.arrivalTime = arrivalTime;
     }
 
-    public int getBurstTime() {
+    public double getBurstTime() {
         return burstTime;
     }
 
-    public void setBurstTime(int burstTime) {
+    public void setBurstTime(double burstTime) {
         this.burstTime = burstTime;
     }
 
-    public int getWaitingTime() {
+    public double getWaitingTime() {
         return waitingTime;
     }
 
-    public void setWaitingTime(int waitingTime) {
-        this.waitingTime = waitingTime;
+    public void setWaitingTime() {
+        this.waitingTime = this.turnaroundTime - burstTime;
     }
 
-    public int getStartTime() {
+    public double getStartTime() {
         return startTime;
     }
 
-    public void setStartTime(int startTime) {
+    public void setStartTime(double startTime) {
         this.startTime = startTime;
+    }
+
+    public double getTurnaroundTime() {
+        return turnaroundTime;
+    }
+
+    public void setTurnaroundTime() {
+        this.turnaroundTime = this.completionTime - this.arrivalTime;
+    }
+
+    public double getCompletionTime() {
+        return completionTime;
+    }
+
+    public void setCompletionTime() {
+        this.completionTime = this.startTime + this.burstTime;
     }
 
     @Override
